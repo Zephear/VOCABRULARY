2f2d0550ac3bc2fe078e6a3b0b34db4997833224
Vladyslav Dovhyi <dovhuy5@gmail.com>
2025-11-18 00:41:01 +0100

    готова фільтрація переключання вдображення термінів і третя сторінка збережене на початковій стадії

--- Backend/app.py
+++ Backend/app.py
@@ -1,16 +1,18 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import sqlite3
-
+import os 
 app = Flask(__name__)
 CORS(app)
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DB_PATH = os.path.join(BASE_DIR, '..', 'Database', 'Database.db')
 
 @app.route('/api/terms')
 def get_terms():
-    conn = sqlite3.connect('../Database/Database.db') 
+    conn = sqlite3.connect(DB_PATH) 
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Definitions")
+    cursor.execute("SELECT * FROM Definitions") 
     rows = cursor.fetchall()
     conn.close()
 
@@ -19,4 +21,3 @@ def get_terms():
 
 if __name__ == '__main__':
     app.run(port=3001, debug=True)
-
